4934376d03d015351902edae794fac227fe5aec1
Louis Royer <infos.louis.royer@gmail.com>
2026-08-17 15:45:47 +0200

    Refactor formatter

--- logger/logger.go
+++ logger/logger.go
@@ -12,39 +12,33 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-type logFormatter struct {
-	logrus.TextFormatter
-}
-
 // Customized log formatter
-func newLogFormatter() *logFormatter {
-	return &logFormatter{
-		TextFormatter: logrus.TextFormatter{
-			ForceColors:            true,
-			FullTimestamp:          true,
-			DisableTimestamp:       false,
-			DisableLevelTruncation: true,
-			PadLevelText:           true,
-			SortingFunc: func(keys []string) {
-				sort.Slice(keys, func(i, j int) bool {
-					// returns true if i is before j
-					// error always at the end
-					if keys[j] == "error" {
-						return true
-					}
-					if keys[i] == "error" {
-						return false
-					}
-					// app always at the begin
-					if keys[j] == "app" {
-						return false
-					}
-					if keys[i] == "app" {
-						return true
-					}
-					return strings.Compare(keys[i], keys[j]) == -1
-				})
-			},
+func newLogFormatter() *logrus.TextFormatter {
+	return &logrus.TextFormatter{
+		ForceColors:            true,
+		FullTimestamp:          true,
+		DisableTimestamp:       false,
+		DisableLevelTruncation: true,
+		PadLevelText:           true,
+		SortingFunc: func(keys []string) {
+			sort.Slice(keys, func(i, j int) bool {
+				// returns true if i is before j
+				// error always at the end
+				if keys[j] == "error" {
+					return true
+				}
+				if keys[i] == "error" {
+					return false
+				}
+				// app always at the begin
+				if keys[j] == "app" {
+					return false
+				}
+				if keys[i] == "app" {
+					return true
+				}
+				return strings.Compare(keys[i], keys[j]) == -1
+			})
 		},
 	}
 }
